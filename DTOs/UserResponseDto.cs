6d6ae279165a8f8a00672545c479c66941898788
Luciano Júnior <luciano.menezes.jr11@gmail.com>
2026-03-20 21:23:13 -0300

    feat(backend-initialize): Add project.nuget.cache file with resolved package dependencies and warnings

---
+++ DTOs/UserResponseDto.cs
@@ -0,0 +1,10 @@
+namespace LabControlApi.DTOs
+{
+	public class UserResponseDto
+	{
+		public int Id { get; set; }
+	public string? Name { get; set; }
+	public string? Email { get; set; }
+		public DateTime CreatedAt { get; set; }
+	}
+}
---
+++ Repositories/Interfaces/IUserRepository.cs
@@ -0,0 +1,16 @@
+
+using LabControlApi.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace LabControlApi.Repositories.Interfaces
+{
+	public interface IUserRepository
+	{
+		Task<IEnumerable<User>> GetAllAsync();
+		Task<User?> GetByIdAsync(int id);
+		Task<User> AddAsync(User user);
+		Task<User?> UpdateAsync(User user);
+		Task<bool> DeleteAsync(int id);
+	}
+}
